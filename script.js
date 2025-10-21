49224482d8f8cba0862449b5f5f5c93f08087564
Muhamad Yusuf <muhamadyusuf0012@gmail.com>
2025-10-21 14:47:47 +0700

    initial

---
+++ script.js
@@ -0,0 +1,5 @@
+function kirim(){
+    let namaDepan = document.getElementById("namaDepan").value
+    let namaBelakang = document.getElementById("namaBelakang").value
+    console.log("Nama Lengkap: " + namaDepan + " " + namaBelakang)
+}
